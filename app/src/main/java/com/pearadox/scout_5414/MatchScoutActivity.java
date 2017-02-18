9a67ab1f30bdd6fed32ad88de6ab97706d33d432
willthisusernamework <matthewm2020@gmail.com>
2017-02-18 16:18:26 -0600

    Added Drop Down Menus

--- app/src/main/java/com/pearadox/scout_5414/MatchScoutActivity.java
+++ app/src/main/java/com/pearadox/scout_5414/MatchScoutActivity.java
@@ -6,8 +6,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -18,6 +25,8 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.Iterator;
 
+import static android.view.View.VISIBLE;
+
 
 public class MatchScoutActivity extends AppCompatActivity {
 
@@ -26,6 +35,8 @@ public class MatchScoutActivity extends AppCompatActivity {
     public static String device = " ";
     public static String studID = " ";
     TextView txt_dev, txt_stud, txt_Match, txt_MyTeam;
+    CheckBox chk_baseline, chk_highGoal, chkBox_balls, chkBox_gears, chkBox_rope, chk_lowGoal;
+    SeekBar seekBar_HighGoal, seekBar_LowGoal;
     ImageView imgScoutLogo;
     public String matchID = "T00";      // Type + #
     String team_num, team_name, team_loc;
@@ -40,6 +51,13 @@ public class MatchScoutActivity extends AppCompatActivity {
     private Button button_GearsMinus, button_GearsPlus, button_GoToTeleopActivity, button_GoToArenaLayoutActivity, button_GoToOtherActivityFromAuto;
     int gearNum = 0;
     String key = null;
+    ArrayAdapter<String> adapter_autostartpos;
+    ArrayAdapter<String> adapter_autostoppos;
+    public String startPos = " ";
+    public String stopPos = " ";
+
+
+
 
 
     //    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
@@ -61,12 +79,118 @@ public class MatchScoutActivity extends AppCompatActivity {
         pfCur_Match_DBReference = pfDatabase.getReference("current-match"); // _THE_ current Match
         pfDevice_DBReference = pfDatabase.getReference("devices");          // List of Students
         txt_GearsPlaced = (TextView) findViewById(R.id.txt_GearsPlaced);
+        chk_baseline = (CheckBox) findViewById(R.id.chk_baseline);
+        chk_highGoal = (CheckBox) findViewById(R.id.chk_highGoal);
+        chk_lowGoal = (CheckBox) findViewById(R.id.chk_LowGoal);
+        seekBar_HighGoal = (SeekBar) findViewById(R.id.seekBar_HighGoal);
+        seekBar_LowGoal = (SeekBar) findViewById(R.id.seekBar_LowGoal);
+        chkBox_balls = (CheckBox) findViewById(R.id.chk_balls);
+        chkBox_gears = (CheckBox) findViewById(R.id.chk_gears);
+        chkBox_rope = (CheckBox) findViewById(R.id.chk_rope);
         button_GearsMinus = (Button) findViewById(R.id.button_GearsMinus);
         button_GearsPlus = (Button) findViewById(R.id.button_GearsPlus);
         button_GoToTeleopActivity = (Button) findViewById(R.id.button_GoToTeleopActivity);
         button_GoToArenaLayoutActivity = (Button) findViewById(R.id.button_GoToArenaLayoutActivity);
         button_GoToOtherActivityFromAuto = (Button) findViewById(R.id.button_GoToFinalActivity);
         txt_GearsPlaced.setText(Integer.toString(gearNum));
+        seekBar_HighGoal.setEnabled(false);
+        seekBar_HighGoal.setVisibility(View.GONE);
+        chk_highGoal.setChecked(false);
+        seekBar_LowGoal.setEnabled(false);
+        seekBar_LowGoal.setVisibility(View.GONE);
+        chk_lowGoal.setChecked(false);
+
+        Spinner spinner_startPos = (Spinner) findViewById(R.id.spinner_startPos);
+        String[] autostartPos = getResources().getStringArray(R.array.auto_start_array);
+        adapter_autostartpos = new ArrayAdapter<String>(this, R.layout.dev_list_layout, autostartPos);
+        adapter_autostartpos.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_startPos.setAdapter(adapter_autostartpos);
+        spinner_startPos.setSelection(0, false);
+        spinner_startPos.setOnItemSelectedListener(new MatchScoutActivity.startPosOnClickListener());
+
+        Spinner spinner_stopPos = (Spinner) findViewById(R.id.spinner_stopPos);
+        String[] autostopPos = getResources().getStringArray(R.array.auto_stop_array);
+        adapter_autostoppos = new ArrayAdapter<String>(this, R.layout.dev_list_layout, autostopPos);
+        adapter_autostoppos.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_stopPos.setAdapter(adapter_autostoppos);
+        spinner_stopPos.setSelection(0, false);
+        spinner_stopPos.setOnItemSelectedListener(new MatchScoutActivity.stopPosOnClickListener());
+
+
+
+        chk_baseline.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
+                Log.i(TAG, "chk_baseline Listener");
+            }
+        }
+        );
+        chkBox_balls.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
+                Log.i(TAG, "chkBox_balls Listener");
+            }
+        }
+        );
+        chkBox_gears.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
+                Log.i(TAG, "chkBox_gears Listener");
+            }
+        }
+        );
+        chkBox_rope.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
+                Log.i(TAG, "chkBox_rope Listener");
+            }
+        }
+        );
+        chk_highGoal.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
+                Log.i(TAG, "chk_highGoal Listener");
+                if (buttonView.isChecked()) {
+                    //checked
+                    seekBar_HighGoal.setEnabled(true);
+                    seekBar_HighGoal.setVisibility(View.VISIBLE);
+                }
+                else
+                {
+                    //not checked
+                    seekBar_HighGoal.setEnabled(false);
+                    seekBar_HighGoal.setVisibility(View.GONE);
+
+                }
+            }
+        }
+        );
+        chk_lowGoal.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
+                Log.i(TAG, "chk_lowGoal Listener");
+                if (buttonView.isChecked()) {
+                    //checked
+                    seekBar_LowGoal.setEnabled(true);
+                    seekBar_LowGoal.setVisibility(View.VISIBLE);
+                }
+                else
+                {
+                    //not checked
+                    seekBar_LowGoal.setEnabled(false);
+                    seekBar_LowGoal.setVisibility(View.GONE);
+
+                }
+            }
+        }
+        );
+
 
         button_GearsPlus.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -242,6 +366,29 @@ public class MatchScoutActivity extends AppCompatActivity {
                 Log.d(TAG, "DEV = NULL" );
         }
              pfDevice_DBReference.child(key).child("phase").setValue(phase);
+    }
+
+    private class startPosOnClickListener implements android.widget.AdapterView.OnItemSelectedListener {
+        public void onItemSelected(AdapterView<?> parent,
+                                   View view, int pos, long id) {
+            startPos = parent.getItemAtPosition(pos).toString();
+            Log.d(TAG, ">>>>>  '" + startPos + "'");
+
+        }
+        public void onNothingSelected(AdapterView<?> parent) {
+            // Do nothing.
+        }
+    }
+    private class stopPosOnClickListener implements android.widget.AdapterView.OnItemSelectedListener {
+        public void onItemSelected(AdapterView<?> parent,
+                                   View view, int pos, long id) {
+            stopPos = parent.getItemAtPosition(pos).toString();
+            Log.d(TAG, ">>>>>  '" + stopPos + "'");
+
+        }
+        public void onNothingSelected(AdapterView<?> parent) {
+            // Do nothing.
+        }
     }
 
 
