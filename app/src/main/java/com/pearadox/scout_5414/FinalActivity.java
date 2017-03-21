a3ebbfbe5e7127b6a24088dd9e298ba8c408b57d
GaleFrench <gale.french@gmail.com>
2017-03-21 18:33:28 -0500

    Correct file name for Final SD card ('-' instead of '_')

--- app/src/main/java/com/pearadox/scout_5414/FinalActivity.java
+++ app/src/main/java/com/pearadox/scout_5414/FinalActivity.java
@@ -323,7 +323,7 @@ public class FinalActivity extends Activity {
 
     private void saveDatatoSDcard() {
         Log.i(TAG, "@@@@  saveDatatoSDcard  @@@@");
-        String filename = Pearadox.Match_Data.getMatch() + "_" + Pearadox.Match_Data.getTeam_num() + ".dat";
+        String filename = Pearadox.Match_Data.getMatch() + "-" + Pearadox.Match_Data.getTeam_num() + ".dat";
         ObjectOutput out = null;
         File directMatch = new File(Environment.getExternalStorageDirectory() + "/download/FRC5414/match/" + Pearadox.FRC_Event + "/" + filename);
         Log.d(TAG, "SD card Path = " + directMatch);
