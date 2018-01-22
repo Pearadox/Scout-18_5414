443ccbe666fb9eb5f353880b55a9a3587416f552
Gale L. French <gale.french@gmail.com>
2018-01-22 16:16:21 -0600

    Match Data Object changes

--- app/src/main/java/com/pearadox/scout_5414/Pearadox.java
+++ app/src/main/java/com/pearadox/scout_5414/Pearadox.java
@@ -85,10 +85,7 @@ public class Pearadox {
     private static String  final_dateTime;          // Date & Time data was saved
 
 // Java Object _SHARED_ by Auto, Tele & Final
-    public static matchData Match_Data = new matchData(match, team_num,
-        auto_mode, auto_rope, auto_carry_fuel, auto_fuel_amount, auto_gear, auto_gears_placed, auto_gears_attempt, auto_baseline, auto_hg, auto_hg_percent, auto_lg, auto_lg_percent, auto_start, auto_stop, auto_gear_pos, auto_act_hopper, auto_fuel_collected, auto_comment,
-        tele_gears_placed, tele_gears_attempt, tele_gear_pickup, tele_hg, tele_hg_percent, tele_lg, tele_lg_percent, tele_cycles, tele_touch_act, tele_touch_pts, tele_climb_attempt, tele_climb_success, tele_comment,
-        final_lostParts, final_lostComms, final_defense_good, final_def_Lane, final_def_Block, final_def_Hopper, final_def_Gear, final_num_Penalties, final_comment, final_studID, final_dateTime);
+    public static matchData Match_Data = new matchData();
 
 // -----  Array of Match Data Objects for Match Data Visualizer
 public static ArrayList<matchData> Matches_Data = new ArrayList<matchData>();
