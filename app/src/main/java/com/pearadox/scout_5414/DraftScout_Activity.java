cb293dba0bfbd83a81da84bf774500a07d4f7404
Gale L. French <gale.french@gmail.com>
2018-03-21 11:20:13 -0500

    add Screenshot capture to Visualizer

--- app/src/main/java/com/pearadox/scout_5414/DraftScout_Activity.java
+++ app/src/main/java/com/pearadox/scout_5414/DraftScout_Activity.java
@@ -1054,3 +1054,4 @@ public void onDestroy() {
     }
 }
 
+//
--- app/src/main/java/com/pearadox/scout_5414/Visualizer_Activity.java
+++ app/src/main/java/com/pearadox/scout_5414/Visualizer_Activity.java
@@ -185,6 +185,7 @@ public class Visualizer_Activity extends AppCompatActivity {
         });
     }
 
+    //===================================================================
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
